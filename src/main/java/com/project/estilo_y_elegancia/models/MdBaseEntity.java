8beac48281a39e6477506806a6f649d1cfb625c0
JeancaDeve <khevinelpro@gmail.com>
2024-05-25 22:39:54 -0500

    Id autogenerado

--- src/main/java/com/project/estilo_y_elegancia/models/MdBaseEntity.java
+++ src/main/java/com/project/estilo_y_elegancia/models/MdBaseEntity.java
@@ -2,6 +2,8 @@ package com.project.estilo_y_elegancia.models;
 
 import java.io.Serializable;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import lombok.AllArgsConstructor;
@@ -14,5 +16,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class MdBaseEntity implements Serializable {
  @Id
+ @GeneratedValue(strategy = GenerationType.IDENTITY)
  private long id;
 }
