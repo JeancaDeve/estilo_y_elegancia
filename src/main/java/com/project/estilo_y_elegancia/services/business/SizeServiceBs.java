e29711edc16ab573853f18835b1c35453ca7ae79
JeancaDeve <khevinelpro@gmail.com>
2024-05-25 20:00:13 -0500

    Se elimino la entidad Size

--- src/main/java/com/project/estilo_y_elegancia/services/business/SizeServiceBs.java
+++
@@ -1,7 +0,0 @@
-package com.project.estilo_y_elegancia.services.business;
-
-import com.project.estilo_y_elegancia.models.MdSize;
-
-public interface SizeServiceBs extends BaseServiceBs<MdSize,Long> {
- 
-}
