9d5afa4a4e5d9a1740b70cbc0ecac241225aea8f
JeancaDeve <khevinelpro@gmail.com>
2024-05-25 22:39:39 -0500

    Enums para categoria y talla

---
+++ src/main/java/com/project/estilo_y_elegancia/enums/EnCategory.java
@@ -0,0 +1,10 @@
+package com.project.estilo_y_elegancia.enums;
+
+public enum EnCategory {
+ CAMISETAS,
+ PANTALONES,
+ VESTIDOS,
+ ABRIGOS,
+ ZAPATOS,
+ ACCESORIOS
+}
---
+++ src/main/java/com/project/estilo_y_elegancia/enums/EnSize.java
@@ -0,0 +1,10 @@
+package com.project.estilo_y_elegancia.enums;
+
+public enum EnSize {
+ XS,
+ S,
+ M,
+ L,
+ XL,
+ XXL
+}
