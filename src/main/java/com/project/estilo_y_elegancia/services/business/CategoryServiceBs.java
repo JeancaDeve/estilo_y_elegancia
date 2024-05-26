b8f01398d9b67731120b62599cc0edeb927acfbf
JeancaDeve <khevinelpro@gmail.com>
2024-05-25 22:38:26 -0500

    Se elimino el modelo categoria y demas

--- src/main/java/com/project/estilo_y_elegancia/services/business/CategoryServiceBs.java
+++
@@ -1,7 +0,0 @@
-package com.project.estilo_y_elegancia.services.business;
-
-import com.project.estilo_y_elegancia.models.MdCategory;
-
-public interface CategoryServiceBs extends BaseServiceBs<MdCategory, Long> {
-
-}
