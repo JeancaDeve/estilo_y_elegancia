c23bae0856aa6a2564ca114e957bc0e2968de186
JeancaDeve <khevinelpro@gmail.com>
2024-05-26 19:28:23 -0500

    @Query para buscar un cliente

--- src/main/java/com/project/estilo_y_elegancia/repositories/SaleRepository.java
+++ src/main/java/com/project/estilo_y_elegancia/repositories/SaleRepository.java
@@ -1,10 +1,15 @@
 package com.project.estilo_y_elegancia.repositories;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import com.project.estilo_y_elegancia.models.MdCustomer;
 import com.project.estilo_y_elegancia.models.MdSale;
 
 @Repository
 public interface SaleRepository extends BaseRepository<Long, MdSale> {
+
+ @Query("SELECT c FROM MdCustomer c WHERE c.name = :#{#customer.name} AND c.lastName = :#{#customer.lastName} AND c.email = :#{#customer.email} AND c.phoneNumber = :#{#customer.phoneNumber}")
+ boolean CustomerExists(MdCustomer customer);
 
 }
