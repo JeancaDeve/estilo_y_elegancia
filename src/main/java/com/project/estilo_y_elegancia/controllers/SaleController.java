723139af76924dc2660f132adfadb700f0b5fb3e
JeancaDeve <khevinelpro@gmail.com>
2024-05-26 19:27:24 -0500

    Se agrego metodo para generar la venta

--- src/main/java/com/project/estilo_y_elegancia/controllers/SaleController.java
+++ src/main/java/com/project/estilo_y_elegancia/controllers/SaleController.java
@@ -1,5 +1,8 @@
 package com.project.estilo_y_elegancia.controllers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +12,10 @@ import com.project.estilo_y_elegancia.services.implementation.SaleServiceImpl;
 @RestController
 @RequestMapping("/api/sale")
 public class SaleController extends BaseController<MdSale, SaleServiceImpl> {
+
+ @PostMapping("/generate")
+ public ResponseEntity<MdSale> generateSale(@RequestBody MdSale sale) {
+  return ResponseEntity.ok(service.generateSale(sale));
+ }
 
 }
