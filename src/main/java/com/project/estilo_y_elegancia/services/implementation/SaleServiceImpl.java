1562b625265e4bccf902c938ba1b056600766ee8
JeancaDeve <khevinelpro@gmail.com>
2024-05-27 11:11:03 -0500

    Se implementaron y mejoraron metodos

--- src/main/java/com/project/estilo_y_elegancia/services/implementation/SaleServiceImpl.java
+++ src/main/java/com/project/estilo_y_elegancia/services/implementation/SaleServiceImpl.java
@@ -1,8 +1,12 @@
 package com.project.estilo_y_elegancia.services.implementation;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.project.estilo_y_elegancia.models.MdClothes;
 import com.project.estilo_y_elegancia.models.MdCustomer;
 import com.project.estilo_y_elegancia.models.MdSale;
 import com.project.estilo_y_elegancia.repositories.BaseRepository;
@@ -14,21 +18,58 @@ import jakarta.transaction.Transactional;
 @Service
 public class SaleServiceImpl extends BaseServiceImpl<MdSale, Long> implements SaleServiceBs {
 
- @Autowired
- protected SaleRepository _SaleRepository;
+  @Autowired
+  protected SaleRepository _SaleRepository;
 
- public SaleServiceImpl(BaseRepository<Long, MdSale> repository) {
-  super(repository);
- }
+  public SaleServiceImpl(BaseRepository<Long, MdSale> repository) {
+    super(repository);
+  }
 
- public boolean CustomerExists(MdCustomer customer) {
-  return _SaleRepository.CustomerExists(customer);
- }
+  private boolean CustomerExists(MdCustomer customer) {
+    return _SaleRepository.CustomerExists(customer).isPresent();
+  }
 
- @Override
- @Transactional
- public MdSale generateSale(MdSale sale) {
-  return _SaleRepository.save(sale);
- }
+  @Override
+  @Transactional
+  public MdSale generateSale(MdSale sale) throws Exception {
+
+    MdCustomer customer = sale.getCustomer();
+
+    // Si el cliente no existe se asegura que no se cree un cliente con el mismo
+    // numero
+    if (!CustomerExists(customer))
+      CustomerExistsByPhoneNumber(customer.getPhoneNumber());
+
+    List<MdClothes> listClothes = sale.getClothes();
+
+    MdCustomer customerSale = CustomerExists(customer) ? _SaleRepository.CustomerExists(customer).get() : customer;
+
+    sale.setTotal(getTotalAmountSale(listClothes));
+    sale.setCant(listClothes.size());
+    sale.setCustomer(customerSale);
+    return _SaleRepository.save(sale);
+
+  }
+
+  private double getTotalAmountSale(List<MdClothes> clothes) {
+
+    List<MdClothes> clothesList = new ArrayList<>();
+
+    List<Long> idList = clothes.stream().map(MdClothes::getId).toList();
+
+    for (Long id : idList) {
+      clothesList.add(_SaleRepository.getClothesById(id));
+    }
+
+    return clothesList.stream().mapToDouble(MdClothes::getPrice).sum();
+  }
+
+  private void CustomerExistsByPhoneNumber(String phoneNumber) throws Exception {
+    if (_SaleRepository.getCustomerByPhoneNumber(phoneNumber).isPresent()) {
+      throw new Exception("El numero " + phoneNumber +
+          " ya esta registrado, y no le pertenece.");
+    }
+
+  }
 
 }
