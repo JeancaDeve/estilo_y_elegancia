61d8a61e91d9056c705640c7f7b63beb526a4f6a
JeancaDeve <khevinelpro@gmail.com>
2024-05-27 11:10:24 -0500

    Nuevos metodos

--- src/main/java/com/project/estilo_y_elegancia/repositories/SaleRepository.java
+++ src/main/java/com/project/estilo_y_elegancia/repositories/SaleRepository.java
@@ -1,15 +1,24 @@
 package com.project.estilo_y_elegancia.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import com.project.estilo_y_elegancia.models.MdClothes;
 import com.project.estilo_y_elegancia.models.MdCustomer;
 import com.project.estilo_y_elegancia.models.MdSale;
 
 @Repository
 public interface SaleRepository extends BaseRepository<Long, MdSale> {
 
- @Query("SELECT c FROM MdCustomer c WHERE c.name = :#{#customer.name} AND c.lastName = :#{#customer.lastName} AND c.email = :#{#customer.email} AND c.phoneNumber = :#{#customer.phoneNumber}")
- boolean CustomerExists(MdCustomer customer);
+ @Query("SELECT c FROM MdCustomer c WHERE c.name = :#{#customer.name} AND c.lastName = :#{#customer.lastName}  AND c.phoneNumber = :#{#customer.phoneNumber}")
+ Optional<MdCustomer> CustomerExists(MdCustomer customer);
+
+ @Query("SELECT c FROM MdClothes c WHERE c.id = :id")
+ MdClothes getClothesById(Long id);
+
+ @Query("SELECT c FROM MdCustomer c WHERE c.phoneNumber = :phoneNumber")
+ Optional<MdCustomer> getCustomerByPhoneNumber(String phoneNumber);
 
 }
