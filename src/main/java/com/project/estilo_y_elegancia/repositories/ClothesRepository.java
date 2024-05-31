2750a599c569ceb8341714fc6d71ab35375301ec
JeancaDeve <khevinelpro@gmail.com>
2024-05-30 20:50:43 -0500

    Nuevos Metodos

--- src/main/java/com/project/estilo_y_elegancia/repositories/ClothesRepository.java
+++ src/main/java/com/project/estilo_y_elegancia/repositories/ClothesRepository.java
@@ -1,10 +1,24 @@
 package com.project.estilo_y_elegancia.repositories;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.project.estilo_y_elegancia.models.MdClothes;
+import com.project.estilo_y_elegancia.enums.EnCategory;
 
 @Repository
-public interface ClothesRepository extends BaseRepository<Long, MdClothes>{
+public interface ClothesRepository extends BaseRepository<Long, MdClothes> {
+
+ @Query("SELECT c FROM MdClothes c WHERE c.stock > 0")
+ List<MdClothes> getClothesAvailables();
+
+ List<MdClothes> findByCategory(EnCategory category);
+
+ List<MdClothes> findByDescriptionContaining(String description);
+
  
+
+
 }
